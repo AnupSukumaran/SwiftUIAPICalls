5d7c3cea2599b90a56f4369cd5022300843d29be
SukumarAnup <sukumars@pyronix.com>
2022-08-04 21:23:29 +0100

    Initial Commit

---
+++ SwiftUIAPICalls/SwiftUIAPICallsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAPICallsApp.swift
+//  SwiftUIAPICalls
+//
+//  Created by sukumar.sukumaran on 04/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAPICallsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
